c693de18136e60a47390a7d9b0e072ec620b9c1f
Roman <r.ledin.exceedteam@gmail.com>
2021-03-30 14:44:30 +0300

    Added vuelayers map

---
+++ src/components/Map.vue
@@ -0,0 +1,20 @@
+<template>
+  <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" style="height: 400px">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" projection="EPSG:4326"></vl-view>
+      <vl-layer-tile>
+          <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+  </vl-map>
+</template>
+
+<script>
+  export default {
+    data () {
+      return { 
+        zoom: 2,
+        center: [0, 0],
+        rotation: 0,
+      }
+    },
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Map } from 'vuelayers'
+import 'vuelayers/lib/style.css'
+import VueLayers from 'vuelayers'
+
+
+Vue.use(Map, {
+  load: {
+  key: "",
+  libraries: "places", // necessary for places input
+  region: "uk,en"
+}
+});
+
+Vue.use(VueLayers, {
+  dataProjection: 'EPSG:4326',
+})
+  
 
 Vue.config.productionTip = false
 
